77e407e7142cb6fb22d75bd18155dfd02a6cce45
Andreas Doebeli <thobens@gmail.com>
2011-09-09 17:48:22 +0200

    PPJQWIDGETS-2

---
+++ plugins/phplug_ui_widgets_jquery/widgets/dialog/Dialog.php
@@ -0,0 +1,16 @@
+<?php
+namespace phplug\plugins\phplug_ui_widgets_jquery\widgets\dialog;
+
+use phplug\platform\PhplugPlatform;
+
+use phplug\plugins\phplug_ui_widgets\widgets\dialog\AbstractDialog;
+
+class Dialog extends AbstractDialog {
+	
+	public function draw() {
+		$uip = PhplugPlatform::getActiveWorkbench()->getUIProcessor();
+		$uip->assign("dialog", $this);
+		return $uip->process("ch.thobens.templates.widgets.dialog");
+	}
+	
+}
