d3619b70fc5f621e44c8218bf801096f59125379
Andreas Doebeli <thobens@gmail.com>
2011-09-09 17:44:44 +0200

    PPWIDGETS-2

---
+++ plugins/phplug_ui_widgets/widgets/dialog/AbstractDialog.php
@@ -0,0 +1,67 @@
+<?php
+namespace phplug\plugins\phplug_ui_widgets\widgets\dialog;
+
+use phplug\plugins\phplug_ui_widgets\widgets\form\Control;
+
+use phplug\plugins\phplug_ui\ui\Composite;
+
+use phplug\plugins\phplug_ui\ui\IComposite;
+
+abstract class AbstractDialog extends Control implements IDialog {
+	
+	protected $title;
+	
+	protected $composite;
+	
+	protected $modal;
+	
+	protected $width;
+	
+	protected $height;
+	
+	public function getTitle() {
+		return $this->title;
+	}
+	
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+	
+	public function getComposite() {
+		return $this->composite;
+	}
+	
+	public function setComposite(IComposite $composite) {
+		$this->composite = $composite;
+	}
+	
+	public function isModal() {
+		return $this->modal;
+	}
+	
+	public function setModal($modal) {
+		$this->modal = $modal;
+	}
+	
+	public function getHeight() {
+		if(!isset($this->height)) {
+			return "auto";
+		}
+		return $this->height;	
+	}
+	
+	public function setHeight($height) {
+		$this->height = $height;
+	}
+	
+	public function getWidth() {
+		if(!isset($this->width)) {
+			return "auto";
+		}
+		return $this->width;	
+	}
+	
+	public function setWidth($width) {
+		$this->width = $width;
+	}
+}
---
+++ plugins/phplug_ui_widgets/widgets/dialog/IDialog.php
@@ -0,0 +1,19 @@
+<?php
+namespace phplug\plugins\phplug_ui_widgets\widgets\dialog;
+
+use phplug\plugins\phplug_ui\ui\IComposite;
+
+interface IDialog extends IComposite {
+	
+	public function getTitle();
+	
+	public function setTitle($title);
+	
+	public function setComposite(IComposite $composite);
+	
+	public function getComposite();
+	
+	public function setModal($modal);
+	
+	public function isModal();
+}
